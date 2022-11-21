e71fd51d7fa1b4974091e00e66b380cf11636b86
JariSara <57097679+JariSara@users.noreply.github.com>
2022-11-21 19:17:23 +0100

    small changes

---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import background from "my-app\src\Components\test.jpg";
+export default class Body extends Component{
+    render(){
+        return(
+            <div>
+        <span className="font-link">
+            <p class="textClass">
+            body
+            </p>
+        </span>
+        </div>
+        )
+    }
+}
